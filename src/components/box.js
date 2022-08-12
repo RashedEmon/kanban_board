e0a27224d69fa2220c759e73d7577cd0d3d60525
Rashed <rashedulb13@gmail.com>
2022-08-13 01:25:43 +0600

    remove some component and add box component

---
+++ src/components/box.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import Item from "./item";
+
+const Box = ({items,handleDragOver,handleDrop,removeItem,boxtype})=>{
+    const [item,setItem] = useState([]);
+
+    useEffect(()=>{
+        setItem(()=>{
+            return items.filter((item)=> item.status === `${boxtype}`);
+        })
+    },[items,boxtype])
+
+    return (
+        <div className="box" onDrop={(e)=>handleDrop(e,`${boxtype}`)} onDragOver={(e)=>handleDragOver(e)} >
+            <h3 style={{textAlign: "center", backgroundColor: "#f1f1f1", textTransform: "uppercase"}}>{boxtype}</h3>
+            <div className="boxContainer">
+            {item.map((it)=><Item key={it.id} id={it.id} text={it.title} removeItem={removeItem}/>)}
+            </div>
+        </div>
+    )
+}
+export default Box;
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,54 @@
-import {useState} from 'react';
-import Inprogress from './inprogess';
-import Todo from './todo';
-import Done from './done';
+import {useEffect, useState} from 'react';
+// import Inprogress from './inprogess';
+// import Todo from './todo';
+// import Done from './done';
+import Box from './box';
 
 
 const Layout = ()=>{
     const [inp, setInput] = useState("");
-    const [items,setItem] = useState([{}]);
+    const [items,setItem] = useState([]);
 
+    const getData=(url,info)=>{
+        
+        fetch(url,info)
+        .then((res)=> res.json())
+        .then((data)=> setItem(data))
+        .catch((error)=>{
+            console.log("error");
+        })
+        
+    }
+
+    const postData=(url,info)=>{
+        fetch(url,info)
+        .then((res)=> res.json())
+        .then((data)=> console.log(data))
+        .catch((error)=>{
+            console.log(error)
+        })
+    }
+
+    useEffect(()=>{
+        getData("http://127.0.0.1:8000/api/task/get",{
+            method: 'GET',
+            headers:{
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers":"Origin, X-Requested-With, Content-Type, Accept"
+            },
+            // mode: "no-cors"
+        });
+    },[]);
+
+    useEffect(()=>{
+        postData("http://127.0.0.1:8000/api/task/add/",{
+            method: 'POST',
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            },
+            body: JSON.stringify(items),
+        })
+    },[items])
 
     const handleText=(e)=>{
         setInput(e.target.value);
@@ -19,18 +60,20 @@ const Layout = ()=>{
             return false;
         }
         const n= {
-            id: new Date().getTime().toLocaleString(),
-            text: ""+inp,
-            status: "todo"
+            id: new Date().getTime().toString(),
+            title: inp,
+            status: "todo",
+            description: "No"
         };
         setItem((prev)=> [...prev,n]);
         setInput("");
+        // console.log(items)
     }
 
 
     const handleDragOver=(e)=>{
         e.preventDefault();
-        console.log(e);
+        // console.log(e);
     }
 
     const handleDrop = (e,fromWhere)=>{
@@ -45,11 +88,17 @@ const Layout = ()=>{
             return item;
         } );
         setItem(val);
-        console.log(val);
+
     }
 
     const removeItem=(id)=>{
         const val = items.filter((item)=> item.id !== id);
+        console.log(id);
+        fetch(`http://127.0.0.1:8000/api/task/remove/${id}`,{method:'GET'})
+        .then((res)=> res)
+        .then((data)=> console.log(data))
+        .catch((error)=> console.log(error));
+        
         setItem(val);
     }
 
@@ -60,9 +109,13 @@ const Layout = ()=>{
                 <button onClick={()=> handleAdd(inp)}>Add</button>
             </div>
             <div className="boxGroup">
-                <Todo items={items} handleDragOver={handleDragOver} handleDrop={handleDrop} removeItem={removeItem}/>
-                <Inprogress items={items} handleDragOver={handleDragOver} handleDrop={handleDrop} removeItem={removeItem}/>
-                <Done items={items} handleDragOver={handleDragOver} handleDrop={handleDrop} removeItem={removeItem}/>
+                {/* <Todo items={items} handleDragOver={handleDragOver} handleDrop={handleDrop} removeItem={removeItem}/>
+                <Inprogress items={items} handleDragOver={handleDragOver} handleDrop={handleDrop} removeItem={removeItem} boxtype={"inprogess"}/>
+                <Done items={items} handleDragOver={handleDragOver} handleDrop={handleDrop} removeItem={removeItem}/> */}
+
+                <Box items={items} handleDragOver={handleDragOver} handleDrop={handleDrop} removeItem={removeItem} boxtype={"todo"}/>
+                <Box items={items} handleDragOver={handleDragOver} handleDrop={handleDrop} removeItem={removeItem} boxtype={"inprogess"}/>
+                <Box items={items} handleDragOver={handleDragOver} handleDrop={handleDrop} removeItem={removeItem} boxtype={"done"}/>
             </div>
         </div>
     )
